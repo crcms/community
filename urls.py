0fb48da957b2efcd810e6285f1c3657b94fb6769
crcms <crcms@crcms.cn>
2018-08-07 17:29:20 +0800

    Route migrate

--- urls.py
+++ urls.py
@@ -6,9 +6,13 @@ URL routes
 
 '''
 
-from django.urls import path
+from django.urls import include,path
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('questions', views.QuestionViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls)),
 ]
