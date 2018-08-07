0a7cfee10b7defceb38145e80d794208184ac77c
crcms <crcms@crcms.cn>
2018-08-07 20:08:27 +0800

    Return class name

--- serializers.py
+++ serializers.py
@@ -13,5 +13,5 @@ from . import repositories
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta(object):
-        model = repositories.QuestionRepository().new_model()
+        model = repositories.QuestionRepository().new_model().__class__
         fields = ('id', 'title', 'content', 'created_at', 'updated_at', 'browse_num', 'good_num', 'comment_num')
