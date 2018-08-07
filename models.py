79e3f67b89de86e9ad286aa2b5e0ab9dbdd1a131
crcms <crcms@crcms.cn>
2018-08-07 17:28:18 +0800

    User model

--- models.py
+++ models.py
@@ -25,3 +25,18 @@ class Question(models.Model):
         # return 'Id: {id}, Title:{title}'.format(id=self.id, title=self.title)
         # or
         return 'Id: {0}, Title:{1}'.format(self.id, self.title)
+
+
+class User(models.Model):
+    class Meta(object):
+        db_table = 'users'
+        managed = False
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=50)
+    tel = models.CharField(max_length=15)
+    status = models.SmallIntegerField(default=0)
+    ticket = models.CharField(max_length=150)
+    ticket_expired_at = models.BigIntegerField(default=0)
+    deleted_at = models.BigIntegerField(null=True)
