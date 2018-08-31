3ad309b97d0e5b31f39b93ce01f1347f822e40d5
crcms <crcms@crcms.cn>
2018-08-31 13:12:49 +0800

    test

--- views.py
+++ views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpRequest,JsonResponse,HttpResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
 from django.urls import reverse
 from rest_framework import viewsets
 from .serializers import QuestionSerializer
 from .repositories import QuestionRepository
 from urllib.parse import quote
 from django.views.decorators.csrf import csrf_exempt
-
+from crcms.settings import PASSPORT
+from crcms.passports import Passport
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = QuestionRepository().all()
     serializer_class = QuestionSerializer
+
 
 @csrf_exempt
 def passport(request: HttpRequest):
@@ -19,14 +21,23 @@ def passport(request: HttpRequest):
     # print(type(str(request.body,encoding='utf-8')))
     # print(request.body.decode(),123)
     print(request.POST)
-    return JsonResponse({'a':'1'})
+    return JsonResponse({'a': '1'})
 
-def index(request):
+
+def index(request: HttpRequest):
+    passport = Passport()
+    result = passport.user(request.GET['token'])
+    print('=====================')
+    print(result['data'])
+    print('=====================')
     return HttpResponse('Index')
 
+
 def login(request: HttpRequest):
+    host = 'http://' + PASSPORT['host']
+    url = quote('http://127.0.0.1:28080' + reverse('community.index'))
+    app_key = PASSPORT['key']
 
-    url = quote('http://192.168.1.142:28080'+reverse('community.index'))
-
-    return HttpResponse(bytes('<a href="http://laravel.test/login?_redirect='+url+'">login</a>',encoding='UTF-8'))
-
+    return HttpResponse(
+        bytes('<a href="' + host + '/login?_redirect=' + url + '&app_key=' + app_key + '">login</a>',
+              encoding='UTF-8'))
